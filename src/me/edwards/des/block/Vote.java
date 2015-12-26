716232c02d408a6924d19fa7a37e32bf37cfd612
MjolnirCommando <blobman555@gmail.com>
2015-12-26 10:21:57 -0500

    Vote Javadoc updates

--- src/me/edwards/des/block/Vote.java
+++ src/me/edwards/des/block/Vote.java
@@ -2,26 +2,37 @@ package me.edwards.des.block;
 
 import java.nio.ByteBuffer;
 
+// -----------------------------------------------------------------------------
 /**
  * Data structure maintaining a single vote for a single entity. This vote may
- * be for an office or a referendum. Ballots are made up of multiple votes.
+ * be for an office or a referendum. {@link Ballot Ballots} are made up of
+ * multiple votes, making the Vote the most basic unit composing {@link Block
+ * Blocks}.<br>
+ * <br>
  * Created on: Nov 3, 2015 at 10:41:29 AM
  * 
  * @author Matthew Edwards
  */
 public class Vote
 {
+    // ~ Static/Instance variables .............................................
+
     private int    id;
     private String vote;
 
 
+    // ~ Constructors ..........................................................
+
+    // -------------------------------------------------------------------------
     /**
-     * Creates new Vote
+     * Creates a new Vote using the specified ID and vote content.<br>
+     * <br>
+     * Ex: [ID:1,VOTE:Obama]
      * 
      * @param id
      *            ID of the office or referendum being voted on
      * @param vote
-     *            Vote (as a String)
+     *            Vote content (as a String)
      */
     public Vote(int id, String vote)
     {
@@ -30,10 +41,13 @@ public class Vote
     }
 
 
+    // ~ Methods ...............................................................
+
+    // -------------------------------------------------------------------------
     /**
-     * Returns the vote ID
+     * Gets the vote ID of the office or referendum being voted on. Ex: 123
      * 
-     * @return
+     * @return ID of the office or referendum being voted on
      */
     public int getID()
     {
@@ -41,10 +55,11 @@ public class Vote
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns the vote
+     * Returns the vote content. Ex: Obama
      * 
-     * @return
+     * @return Vote content (as a String)
      */
     public String getVote()
     {
@@ -52,10 +67,11 @@ public class Vote
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns this vote as bytes
+     * Returns this vote in binary format as a byte array.
      * 
-     * @return
+     * @return Byte array representing this Vote
      */
     public byte[] getBytes()
     {
