cf704a27d74c6071a3e70e72043ee7db2da919f8
MjolnirCommando <blobman555@gmail.com>
2015-12-26 14:17:33 -0500

    BlockChain Javadoc updates

--- src/me/edwards/des/block/BlockChain.java
+++ src/me/edwards/des/block/BlockChain.java
@@ -1,25 +1,69 @@
+/*============================================================================*\
+ | Copyright (C) 2015 Matthew Edwards                                         |
+ |                                                                            |
+ | Licensed under the Apache License, Version 2.0 (the "License"); you may    |
+ | not use this file except in compliance with the License. You may obtain a  |
+ | copy of the License at                                                     |
+ |                                                                            |
+ |     http://www.apache.org/licenses/LICENSE-2.0                             |
+ |                                                                            |
+ | Unless required by applicable law or agreed to in writing, software        |
+ | distributed under the License is distributed on an "AS IS" BASIS,          |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   |
+ | See the License for the specific language governing permissions and        |
+ | limitations under the License.                                             |
+\*============================================================================*/
+
 package me.edwards.des.block;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
+// -----------------------------------------------------------------------------
 /**
- * Data structure to maintain the entire (or a section of the) block chain
+ * Data structure to maintain {@link Block Blocks} in the BlockChain. Blocks can
+ * be randomly appended to the BlockChain using
+ * {@linkplain BlockChain#append(Block)} and will be put into the correct order
+ * to form a continuous tree of Blocks backwards-linked through the
+ * {@link Block#getPrevHash() previous hash} fields. The BlockChain maintains a
+ * queue of orphan Blocks to ensure that Blocks received by the Node
+ * out-of-order in the tree can be appended to the BlockChain correctly. The
+ * BlockChain also maintains a list of all the Blocks acting as leaves on the
+ * top of the tree, but will always choose the longest continuous chain of
+ * Blocks as the main chain.<br>
+ * <br>
+ * The branching abilities of the BlockChain provide the mechanism of the
+ * network to "vote" on valid Blocks and come to a consensus of what the "real"
+ * BlockChain is. Blocks that are generated and deemed valid are added to the
+ * BlockChain while invalid Blocks will never be added to the BlockChain. When
+ * two valid Blocks with the same parent are added to the BlockChain, a branch
+ * is created, which may cause a split in the network. Further explanation
+ * provided with {@linkplain BlockChain#append(Block)}.<br>
+ * <br>
  * Created on: Nov 2, 2015 at 2:23:14 PM
  * 
  * @author Matthew Edwards
  */
 public class BlockChain
 {
+    // ~ Static/Instance variables .............................................
+
     private Node             top;
     private ArrayList<Block> queue;
     private ArrayList<Node>  topList;
 
 
+    // ~ Constructors ..........................................................
+
+    // -------------------------------------------------------------------------
     /**
-     * Creates new BlockChain from the genesis block
+     * Creates new BlockChain with the first {@link Block Block} (the Genesis
+     * Block). This constructor is only used when the Node is starting a new
+     * BlockChain, otherwise the BlockChain is downloaded from a peer Node or
+     * loaded from file.
      * 
      * @param genesis
+     *            First Block in the new BlockChain
      */
     public BlockChain(Block genesis)
     {
@@ -32,11 +76,17 @@ public class BlockChain
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Creates new BlockChain from binary data
+     * Initializes BlockChain from binary data as a byte array. This constructor
+     * is used when a BlockChain is loaded from a file or downloaded from a peer
+     * Node.
      * 
      * @param size
+     *            Number of chunks in which this BlockChain was saved
      * @param binary
+     *            Loaded array of chunks (byte arrays containing {@link Block
+     *            Blocks}) representing this BlockChain
      */
     public BlockChain(int size, byte[][] binary)
     {
@@ -71,10 +121,14 @@ public class BlockChain
     }
 
 
+    // ~ Methods ...............................................................
+
+    // -------------------------------------------------------------------------
     /**
-     * Returns this BlockChain in binary format
+     * Returns the BlockChain in binary format as a chunk array.
      * 
-     * @return
+     * @return Array of chunks (byte arrays containing {@link Block Blocks})
+     *         representing this BlockChain
      */
     public byte[][] getBytes()
     {
@@ -112,10 +166,12 @@ public class BlockChain
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns the block on the top of the BlockChain
+     * Returns the {@link Block Block} on the top of the longest branch in this
+     * BlockChain.
      * 
-     * @return
+     * @return Block at the top of this BlockChain
      */
     public Block getTop()
     {
@@ -123,10 +179,12 @@ public class BlockChain
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns the number of nodes in this BlockChain
+     * Returns the number of {@link Block Blocks} in the longest branch in this
+     * BlockChain
      * 
-     * @return
+     * @return Number of Blocks in the longest branch
      */
     public int getSize()
     {
@@ -134,10 +192,37 @@ public class BlockChain
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Queues the specified block to be added to this BlockChain
+     * Queues the specified {@link Block Block} to be added to this BlockChain.
+     * This method accepts orphan Blocks (a Block whose parent is not contained
+     * in the BlockChain), child Blocks of the main chain, and child Blocks of
+     * branch chains. The algorithm for appending Blocks allows for branching of
+     * the BlockChain, and resolution of Blocks to form a consensus between
+     * Nodes.<br>
+     * <br>
+     * For example, if a Node in California generated a valid Block at the same
+     * time as a Node in Virginia, Nodes on the East Coast might have a
+     * different version of the BlockChain than Nodes on the West Coast. The
+     * East Coast would have the Block generated by the Node in Virginia as the
+     * top block in the BlockChain, while the West Coast would have the Block
+     * generated by the Node in California as the top block in the BlockChain.
+     * However, all Nodes would have both the "East Block" and the "West Block"
+     * contained in their BlockChains, just disagree on which Block is the at
+     * the top of the BlockChain.<br>
+     * <br>
+     * When the next Block is generated and propagated throughout the network,
+     * it will have either the "East Block" or the "West Block" as a parent. As
+     * Nodes receive this new Block (which as an example will have the
+     * "East Block" as its parent), the branch containing the parent will be
+     * extended, and the entire network will come to a consensus concerning the
+     * BlockChain. The new block will be at the top of the BlockChain and the
+     * "East Block" will be its parent. The branch containing the "West Block"
+     * is now shorter, and declared invalid by each Node. Therefore, the entire
+     * network will have a single agreed-upon BlockChain.
      * 
      * @param block
+     *            Block to be added to this BlockChain
      */
     public void append(Block block)
     {
@@ -202,7 +287,7 @@ public class BlockChain
         Node temp = top;
         for (int i = 0; topList.size() > i; i++)
         {
-            if (topList.get(i).height + 4 < getSize())
+            if (topList.get(i).height + 11 < getSize())
             {
                 topList.remove(i);
                 i--;
@@ -213,95 +298,117 @@ public class BlockChain
                 temp = topList.get(i);
             }
         }
+        topList.remove(top);
+        topList.add(0, top);
         top = temp;
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns true if the specified hash belongs to a block in this BlockChain
+     * Returns true if the specified hash belongs to a {@link Block Block} in
+     * this BlockChain.
      * 
      * @param hash
-     * @return
+     *            Block's 32-digit hexadecimal {@link Block#getHash() hash}
+     * @return True if the specified hash belongs to a Block in this BlockChain
      */
     public boolean contains(String hash)
     {
-        Node n = top;
-        while (n.height >= 0)
+        for (int i = 0; topList.size() > i; i++)
         {
-            if (n.block.getHash().equalsIgnoreCase(hash))
+            Node n = topList.get(i);
+            while (n.height >= 0)
             {
-                return true;
-            }
-            if (n.height > 0)
-            {
-                n = n.parent;
-            }
-            else
-            {
-                break;
+                if (n.block.getHash().equalsIgnoreCase(hash))
+                {
+                    return true;
+                }
+                if (n.height > 0)
+                {
+                    n = n.parent;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
         return false;
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns the Block in this BlockChain with the specified hash
+     * Returns the {@link Block Block} in this BlockChain with the specified
+     * hash.
      * 
      * @param hash
-     * @return
+     *            Block's 32-digit hexadecimal {@link Block#getHash() hash}
+     * @return If the Block exists, it is returned, otherwise, a null value is
+     *         returned.
      */
     public Block get(String hash)
     {
-        Node n = top;
-        while (n.height >= 0)
+        for (int i = 0; topList.size() > i; i++)
         {
-            if (n.block.getHash().equalsIgnoreCase(hash))
+            Node n = topList.get(i);
+            while (n.height >= 0)
             {
-                return n.block;
-            }
-            if (n.height > 0)
-            {
-                n = n.parent;
-            }
-            else
-            {
-                break;
+                if (n.block.getHash().equalsIgnoreCase(hash))
+                {
+                    return n.block;
+                }
+                if (n.height > 0)
+                {
+                    n = n.parent;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
         return null;
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Returns the node in this BlockChain containing the Block with the
-     * specified hash
+     * Returns the {@link Node node} in this BlockChain containing the Block
+     * with the specified hash.
      * 
      * @param hash
-     * @return
+     *            Block's 32-digit hexadecimal {@link Block#getHash() hash}
+     * @return If the Block exists, the node containing it is returned.
+     *         Otherwise, a null value is returned.
      */
     public Node getNode(String hash)
     {
-        Node n = top;
-        while (n.height >= 0)
+        for (int i = 0; topList.size() > i; i++)
         {
-            if (n.block.getHash().equalsIgnoreCase(hash))
+            Node n = topList.get(i);
+            while (n.height >= 0)
             {
-                return n;
-            }
-            if (n.height > 0)
-            {
-                n = n.parent;
-            }
-            else
-            {
-                break;
+                if (n.block.getHash().equalsIgnoreCase(hash))
+                {
+                    return n;
+                }
+                if (n.height > 0)
+                {
+                    n = n.parent;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
         return null;
     }
 
 
+    // -------------------------------------------------------------------------
     @Override
     public String toString()
     {
@@ -330,23 +437,31 @@ public class BlockChain
     }
 
 
+    // -------------------------------------------------------------------------
     /**
-     * Represents a node in the BlockChain Created on: Dec 21, 2015 at 5:20:30
-     * PM
+     * Represents a tree node in the BlockChain<br>
+     * <br>
+     * Created on: Dec 21, 2015 at 5:20:30 PM
      * 
      * @author Matthew Edwards
      */
     public class Node
     {
+        // ~ Static/Instance variables .........................................
+
         private Node  parent;
         private int   height;
         private Block block;
 
 
+        // ~ Methods ...........................................................
+
+        // ---------------------------------------------------------------------
         /**
-         * Returns the parent node
+         * Returns the parent node.
          * 
-         * @return
+         * @return Parent node of this node. If this node is at the bottom of
+         *         the chain, the parent node is null
          */
         public Node getParent()
         {
@@ -354,10 +469,12 @@ public class BlockChain
         }
 
 
+        // ---------------------------------------------------------------------
         /**
-         * Returns the height of this node in the BlockChain
+         * Returns the height of this node in the BlockChain.
          * 
-         * @return
+         * @return Height of this node. The node at the bottom of the chain has
+         *         a height of 0
          */
         public int getHeight()
         {
@@ -365,10 +482,11 @@ public class BlockChain
         }
 
 
+        // ---------------------------------------------------------------------
         /**
-         * Returns the Block contained in this node
+         * Returns the {@link Block Block} contained in this node.
          * 
-         * @return
+         * @return Block contained in this node
          */
         public Block getBlock()
         {
